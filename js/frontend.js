a0506369ab34d5f4af95063725354145404d0775
Finn Dohrn <59482102+bitnulleins@users.noreply.github.com>
2024-10-17 15:35:23 +0200

    Fix error with too long random seq number. Add possibility to upload own QR code via upload.

--- js/frontend.js
+++ js/frontend.js
@@ -1,5 +1,7 @@
 "use strict"
 
+const html5QrCode = new Html5Qrcode("reader");
+
 var textOut = null
 var textIn = ""
 var fields = {}
@@ -7,52 +9,56 @@ var fields = {}
 var run, getDayOfYear
 var ctx = null
 
+var fldDef = {
+	'bookRef': { pad: 7, desc:"Booking Ref"},
+	'from':    { pad: 3, desc:"From"},
+	'to':      { pad: 3, desc:"To"},
+	'fOp':     { pad: 3, desc:"Flight Operator"},
+	'fNum':    { pad: 5, desc:"Flight Number"},
+	'seqNum':  { pad: 4, desc:"Seq Number"}
+}
+
 function start() 
-{
-	update_bookmark()
-	
+{	
 	run = Module.cwrap('run', null, ['number', 'string', 'number', 'number'])
 	ctx = theimg3.getContext('2d')
-	dateEd.value = todayInputValue()
 
-	const params = new Proxy(new URLSearchParams(window.location.search), {
-		get: (searchParams, prop) => searchParams.get(prop),
-	});
-
-	if (params.firstName && params.lastName) {
-		document.getElementById('firstNameEd').value	= params.firstName
-		document.getElementById('lastNameEd').value		= params.lastName
-		document.getElementById('fOpEd').value			= params.flightOps
-		document.getElementById('fNumEd').value			= params.flightnumber
-		document.getElementById('fromEd').value			= params.from
-		document.getElementById('toEd').value			= params.to
-		document.getElementById('seatNumEd').value		= params.seatNum
-		document.getElementById('bookRefEd').value		= params.bookRef
-	}
-
+	formatSeqNum()
 	formatName()
 	formatField('bookRef')
 	formatField('from')
 	formatField('to')
 	formatField('fOp')
 	formatField('fNum')
+	formatField('seqNum')
 	readDate()
 	formatClass()
 	formatSeat()
-	formatSeqNum()
+
+	const urlSearchParams = new URLSearchParams(window.location.search);
+	const params = Object.fromEntries(urlSearchParams.entries());
+	if (Object.keys(params).length > 0) {
+		Object.entries(params).forEach(([k, v]) => fields[k] = v)
+		document.getElementById("dateEd").value = getDateFromDays(fields.date)
+	}
+
+	// Update
 	format()
+	update_bookmark()
 }
 
 function update_bookmark() {
 	let queries = []
-	queries['firstName']		= document.getElementById('firstNameEd').value
-	queries['lastName']			= document.getElementById('lastNameEd').value
-	queries['flightOps']		= document.getElementById('fOpEd').value
-	queries['flightnumber']		= document.getElementById('fNumEd').value
-	queries['from']				= document.getElementById('fromEd').value
-	queries['to']				= document.getElementById('toEd').value
-	queries['seatNum']			= document.getElementById('seatNumEd').value
-	queries['bookRef']			= document.getElementById('bookRefEd').value
+	queries['name']				= fields.name
+	queries['fOp']				= fields.fOp
+	queries['fNum']				= fields.fNum
+	queries['from']				= fields.from
+	queries['to']				= fields.to
+	queries['date']				= fields.date
+	queries['seat']				= fields.seat
+	queries['bookRef']			= fields.bookRef
+	queries['seqNum']			= fields.seqNum
+	queries['cls']				= fields.cls
 
 	let query = Object.entries(queries).map(([k,v]) => `${k}=${v}`)
 	
@@ -102,10 +108,31 @@ function update(fromRaw)
 }
 
 function format() {
-	textIn = "M1" + fields.name + "E" + fields.bookRef + fields.from + fields.to 
-	+ fields.fOp + fields.fNum + fields.date + fields.cls + fields.seat + fields.seqNum
-	+ "1" + "00"
-	// 1 passenger status, 00 - hex length of following data
+	document.getElementById('firstNameEd').value	= fields.name.trim().split('/')[0].toUpperCase()
+	document.getElementById('lastNameEd').value		= fields.name.trim().split('/')[1].toUpperCase()
+	document.getElementById('fOpEd').value			= fields.fOp.toUpperCase()
+	document.getElementById('fNumEd').value			= fields.fNum.toUpperCase()
+	document.getElementById('fromEd').value			= fields.from.toUpperCase()
+	document.getElementById('toEd').value			= fields.to.toUpperCase()
+	document.getElementById('seatNumEd').value		= fields.seat.toUpperCase()
+	document.getElementById('bookRefEd').value		= fields.bookRef.toUpperCase()
+	document.getElementById('seqNumEd').value		= fields.seqNum.toUpperCase()
+	if (fields.cls == 'M') fields.cls = 'Y'
+	document.getElementById('classSel').value		= fields.cls.toUpperCase()
+
+	textIn = "M1"
+		+ upPadRight(fields.name,20)
+		+ "E"
+		+ upPadRight(fields.bookRef, fldDef['bookRef'].pad)
+		+ upPadRight(fields.from, fldDef['from'].pad)
+		+ upPadRight(fields.to, fldDef['to'].pad)
+		+ upPadRight(fields.fOp, fldDef['fOp'].pad)
+		+ upPadRight(fields.fNum, fldDef['fNum'].pad)
+		+ padLeft(fields.date, 3) 
+		+ fields.cls
+		+ padLeft(fields.seat, 4)
+		+ padLeft(fields.seqNum, 4)
+		+ "1" + "00" // 1 passenger status, 00 - hex length of following data
 	update()
 }
 function upPadRight(s, n) {
@@ -122,6 +149,16 @@ function padLeft(s, n) {
 	return s.substr(0,n)
 }
 
+function formatField(name, doRun) {
+	errclear('')
+	var def = fldDef[name]
+	var v = document.getElementById(name + "Ed").value
+	if (v.length > def.pad)
+		err('"' + def.desc + '" longer than ' + def.pad + " chars")
+		fields[name] = v
+		if (doRun) format()
+}
+
 function formatName(doRun) {
 	errclear()
 	if (lastNameEd.value.length > 18) // need space for slash and last name
@@ -129,39 +166,13 @@ function formatName(doRun) {
 		var v = lastNameEd.value + "/" + firstNameEd.value
 		if (v.length > 20)
 			err("first+last name longer than 20 characters")
-
-			fields.name = upPadRight(v, 20)
-			if (doRun)
-				format()
-				}
-
-var fldDef = {
-	'bookRef': { pad: 7, desc:"Booking Ref"},
-	'from':    { pad: 3, desc:"From"},
-	'to':      { pad: 3, desc:"To"},
-	'fOp':     { pad: 3, desc:"Flight Operator"},
-	'fNum':    { pad: 5, desc:"Flight Number"}
+			fields.name = v
+			if (doRun) format()
 }
 
-function formatField(name, doRun) {
-	errclear('')
-	var def = fldDef[name]
-	var v = document.getElementById(name + "Ed").value
-	if (v.length > def.pad)
-		err('"' + def.desc + '" longer than ' + def.pad + " chars")
-		fields[name] = upPadRight(v, def.pad)
-		if (doRun)
-			format()
-			}
-
 function formatDay(doRun) {
-	fields.date = padLeft(dayOfYearEd.value, 3)
-	if (doRun)
-		format()
-		}
-function todayInputValue() { // http://stackoverflow.com/questions/6982692/html5-input-type-date-default-value-to-today
-	var local = new Date();
-	return local.toJSON().slice(0,10);
+	fields.date = dayOfYearEd.value
+	if (doRun) format()
 }
 
 function dayOfYear(yr, mon, mday)
@@ -183,37 +194,66 @@ function readDate(toRun) {
 	dayOfYearEd.value = dayOfYear(yr, mon, mday)
 	formatDay(toRun)
 }
+
+function getDateFromDays(days) {
+	var acutalYear = new Date().getFullYear()
+	return new Date(new Date(acutalYear, 0).setDate(days)).toJSON().slice(0,10)
+}
+
 function formatClass(doRun) {
 	fields.cls = classSel.value
-	if (doRun)
-		format()
-		}
+	if (doRun) format()
+}
 function formatSeat(doRun) {
 	errclear()
 	var n = seatNumEd.value
 	if (n.length > 4)
 		err("Seat longer than 4 chars")
-		fields.seat = padLeft(n, 4)
-		if (doRun)
-			format()
-			}
+		fields.seat = n
+		if (doRun) format()
+}
 function formatSeqNum(doRun) {
 	errclear()
 	var n = "0" + getRandomArbitrary(500,999)
 	if (n.length > 4)
 		err("Seq number longer than 4 chars")    
-		fields.seqNum = upPadRight(padLeft(n, 4), 5)
-		if (doRun)
-			format()
-			}    
+		if (seqNumEd.value == '0000') {
+			document.getElementById('seqNumEd').value = n
+		} else {
+			fields.seqNum = seqNumEd.value
+		}
+		if (doRun) format()
+}    
 
 function typeUpdate() {
 	if (codeTypeSel.value == "0")
 		symSize.value = "11"  // size of the aztec
-		else
-			symSize.value = "6"  // columns in the barcode
-			update()
+	else
+		symSize.value = "6"  // columns in the barcode
+	update()
 }
 function getRandomArbitrary(min, max) {
-	return Math.random() * (max - min) + min;
+	return Math.round(Math.random() * (max - min) + min);
+}
+
+function loadQRCode(doRun) {
+	if (doRun.files.length == 0) return;
+	
+	// Scan QR Code
+	html5QrCode.scanFile(doRun.files[0], false)
+	.then(qr_code_val => {
+		fields.name 	= qr_code_val.substring(2, 22)
+		fields.bookRef 	= qr_code_val.substring(23, 29).trim()
+		fields.from 	= qr_code_val.substring(30, 33).trim()
+		fields.to 		= qr_code_val.substring(33, 36).trim()
+		fields.fOp 		= qr_code_val.substring(36, 39).trim()
+		fields.fNum 	= qr_code_val.substring(39, 44).trim()
+		fields.seat 	= qr_code_val.substring(49, 52).trim()
+		fields.cls 		= qr_code_val.substring(47, 48).trim()
+		fields.date 	= qr_code_val.substring(44, 47).trim()
+		fields.seqNum 	= qr_code_val.substring(52, 56).trim()
+		document.getElementById("dateEd").value = getDateFromDays(fields.date)
+		if (doRun) format()
+	})
+	.catch(err => console.log(`Error scanning file. Reason: ${err}`));
 }
